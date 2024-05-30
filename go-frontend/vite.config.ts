eec5a57e370b53865f4577841992424435a4d767
Timofei <timoffeus@gmail.com>
2024-05-30 15:52:29 -0500

    alias are added

--- go-frontend/vite.config.ts
+++ go-frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import vitePluginSvgr from 'vite-plugin-svgr'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginSvgr(), ]
+  plugins: [react(), vitePluginSvgr(),],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+    }
+  }
 })
